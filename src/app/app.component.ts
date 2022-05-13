6f9cf92dd980dee3966287609c0914d9888a118f
KhongBinhMinh <minhkbph17672@fpt.edu.vn>
2022-05-13 10:21:15 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,47 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'We16304';
+  //Phần logic, định nghĩa giá trị biến 
+  name = 'minhkbph17672';
+  class = 'we16304'
+
+  //Kiểu dữ liệu mảng
+  teachers= [
+    {
+      id:'GV1',
+      name:'Nguyễn Văn A',
+      age:20,
+      gender:1,
+      avatar:'https://picsum.photos/100',
+      status:1
+    },
+    {
+      id:'GV2',
+      name:'Nguyễn Văn B',
+      age:40,
+      gender:1,
+      avatar:'https://picsum.photos/100',
+      status:1
+    },
+    {
+      id:'GV3',
+      name:'Nguyễn Văn C',
+      age:20,
+      gender:0,
+      avatar:'https://picsum.photos/100',
+      status:1
+    },
+    {
+      id:'GV4',
+      name:'Nguyễn Văn D',
+      age:45,
+      gender:0,
+      avatar:'https://picsum.photos/100',
+      status:0
+    },
+  ];
+
+  StudentName='Minhkbph17672'
+  StudentId='PH123'
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { TableComponent } from './table/table.component';
+import { TableNameComponent } from './table-name/table-name.component';
+import { TableGenderComponent } from './table-gender/table-gender.component';
+import { TableStatusComponent } from './table-status/table-status.component';
+import { NameComponent } from './name/name.component';
+import { IdentityComponent } from './identity/identity.component';
+import { TableAvatarComponent } from './table-avatar/table-avatar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TableComponent,
+    TableNameComponent,
+    TableGenderComponent,
+    TableStatusComponent,
+    NameComponent,
+    IdentityComponent,
+    TableAvatarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
